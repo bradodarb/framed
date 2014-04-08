507a70f889c014028095820552b67ef55dde5088
bradodarb <bradodarb@hotmail.com>
2014-04-08 01:45:47 -0700

    implemented dal and api

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+
+    framesApi:{
+
+        baseUrl: '/api/',
+        collection: 'frames',
+        dbUrl: 'mongodb://127.0.0.1:27017/framed',
+        safe: true,
+        strict: false
+    }
+
+
+
+
+};
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-exports = require('./lib');
+var frames = require('framed-api');
+var config = require('./config');
+
+
+var restify = require('restify');
+
+var server = restify.createServer();
+
+server.use(restify.bodyParser({
+    maxBodySize: 0,
+    mapParams: true,
+    mapFiles: false,
+    overrideParams: false
+}));
+
+
+var frameController = new frames.frameAPI(server, config.framesApi);
+
+
+
+console.log(frameController);
+
+server.listen(8083);
+
